e8269f9863d5d6291182134d665fdec176611dca
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-06-11 14:46:02 -0600

    Completed mini project

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-export default function SizeChanger() {
+export default function SizeChanger(props) {
   return (
-    <select className="dropDownContainer">
+    <select className="dropDownContainer" onChange={(e) => {
+        let num = parseInt(e.target.value)
+        props.update(num)
+      }}
+      disabled={props.allowEdit === "false"}
+    >
       <option value="12"> 12 </option>
       <option value="13"> 13 </option>
       <option value="14"> 14 </option>
+      <option value="16"> 16 </option>
+      <option value="18"> 18 </option>
+      <option value="24"> 24 </option>
     </select>
   )
 }
